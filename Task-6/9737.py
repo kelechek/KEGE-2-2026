3ddf3421cf4dc0ce44481375ca226a469101aea7
boris <boris@LAPTOP-655K9220>
2025-10-31 19:51:11 +0200

    Задание 6 теория

---
+++ Task-6/9737.py
@@ -0,0 +1,33 @@
+from turtle import *
+
+screensize(3000, 3000)
+tracer(False)
+m = 25
+
+for i in range(2):
+    fd(10 * m)
+    rt(90)
+    fd(18 * m)
+    rt(90)
+
+up()
+fd(5 * m)
+rt(90)
+fd(7 * m)
+lt(90)
+down()
+
+for i in range(2):
+    fd(10 * m)
+    rt(90)
+    fd(7 * m)
+    rt(90)
+
+up()
+for x in range(-5, 20):
+    for y in range(-20, 5):
+        goto(x * m, y * m)
+        dot(3, 'red')
+
+update()
+done()
